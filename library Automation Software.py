a232bd6db11be9e0b39fcae777dab3757cf378e2
Aniketh Vadthya <69419368+Aniketh077@users.noreply.github.com>
2022-09-14 14:33:29 +0530

    Update library Automation Software.py

--- library Automation Software.py
+++ library Automation Software.py
@@ -1,5 +1,5 @@
 '''
-The Bookshop Automation Software (BAS) is to automate all operations in a bookshop.
+The library managment Automation Software (BAS) is to automate all operations in a bookshop.
 Generally it includes the Order Processing, Stock Management and Accounts Management. Also
 BAS will provide the ability to search any book using the book title or the name of the author
 that are available in the shop and in case where the book is not available in the stock, it will ask
